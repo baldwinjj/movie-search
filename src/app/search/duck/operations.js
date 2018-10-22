af17eba56c0d540c621f1d4a9101a0a389abc461
Joey Baldwin <baldwinjj@g.cofc.edu>
2018-10-22 09:02:07 -0400

    change api key

--- src/app/search/duck/operations.js
+++ src/app/search/duck/operations.js
@@ -1,7 +1,7 @@
 import actions from './actions'
 
 const OMDB_URL = 'https://www.omdbapi.com/'
-const API_KEY = '83336d43'
+const API_KEY = '9f572b90'
 
 const searchMovies = (query, page=1) => (dispatch) => {
     dispatch(actions.searchMoviesRequest())
